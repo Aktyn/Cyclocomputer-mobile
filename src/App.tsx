616236b4210acd2f2c0ecc36663e8e068b68b986
Radosław Krajewski 😊 <Aktyn3@gmail.com>
2022-06-24 00:48:22 +0200

    eslint; prettier; fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,129 @@
-import { useDimensions } from '@react-native-community/hooks';
-import { registerRootComponent } from 'expo';
-import { StatusBar } from 'expo-status-bar';
-import { Provider, Text, ThemeProvider } from 'react-native-paper';
-import { blueGrey } from 'material-ui-colors';
-import React from 'react';
-import { StyleSheet, SafeAreaView, useColorScheme, Platform, StatusBar as StatusBarProps } from 'react-native';
-import { darkTheme } from './themes/darkTheme';
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import {
+  Inter_100Thin,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter'
+import { useDimensions } from '@react-native-community/hooks'
+import { registerRootComponent } from 'expo'
+import * as Font from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { StatusBar } from 'expo-status-bar'
+import {
+  StyleSheet,
+  SafeAreaView,
+  useColorScheme,
+  Platform,
+  StatusBar as StatusBarProps,
+  PlatformOSType,
+} from 'react-native'
+import {
+  Button,
+  Caption,
+  configureFonts,
+  DefaultTheme,
+  Headline,
+  Paragraph,
+  Provider,
+  Subheading,
+  Text,
+  Title,
+} from 'react-native-paper'
+import { Fonts } from 'react-native-paper/lib/typescript/types'
+import { darkTheme } from './themes/darkTheme'
 
-
+const fontConfig: {
+  [platform in PlatformOSType | 'default']?: Fonts
+} = {
+  default: {
+    thin: {
+      fontFamily: 'Inter100',
+      fontWeight: 'normal',
+    },
+    light: {
+      fontFamily: 'Inter300',
+      fontWeight: 'normal',
+    },
+    regular: {
+      fontFamily: 'Inter400',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily: 'Inter500',
+      fontWeight: 'normal',
+    },
+  },
+}
 
 export default function App() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
   const dimensions = useDimensions()
-  
-  console.log('color scheme: ', colorScheme,'dimensions: ', dimensions);
+
+  const [appIsReady, setAppIsReady] = useState(false)
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync()
+        await Font.loadAsync({
+          Inter100: Inter_100Thin,
+          Inter300: Inter_300Light,
+          Inter400: Inter_400Regular,
+          Inter500: Inter_500Medium,
+        })
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error(e)
+      } finally {
+        setAppIsReady(true)
+      }
+    }
+
+    prepare()
+  }, [])
+
+  const theme = useMemo(() => {
+    const fonts = configureFonts(fontConfig)
+
+    return colorScheme === 'dark'
+      ? { ...darkTheme, fonts }
+      : { ...DefaultTheme, fonts }
+  }, [colorScheme])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync()
+    }
+  }, [appIsReady])
+
+  console.log('color scheme: ', colorScheme, 'dimensions: ', dimensions)
+
+  if (!appIsReady) {
+    return null
+  }
+
   return (
-    <Provider>
-      <ThemeProvider theme={darkTheme}>
-        <SafeAreaView style={styles.container}>
-          <Text style={{fontSize: 32}}>Hello there</Text>
-          <StatusBar style="auto" />
-        </SafeAreaView>
-      </ThemeProvider>
+    <Provider theme={theme}>
+      <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+        <StatusBar style="auto" />
+        <Headline>Headline</Headline>
+        <Title>Title</Title>
+        <Subheading>Subheading</Subheading>
+        <Text>Text</Text>
+        <Paragraph>Paragraph</Paragraph>
+        <Caption>Caption</Caption>
+        <Button
+          dark
+          icon="camera"
+          mode="contained"
+          onPress={() => console.log('Tap!')}
+        >
+          Test
+        </Button>
+      </SafeAreaView>
     </Provider>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -31,9 +131,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: darkTheme.colors.background,
     alignItems: 'center',
-    // justifyContent: 'center',
-    paddingTop: Platform.OS === 'android' ? StatusBarProps.currentHeight : 0
+    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBarProps.currentHeight : 0,
   },
-});
+})
 
 registerRootComponent(App)
--- src/themes/darkTheme.ts
+++ src/themes/darkTheme.ts
@@ -1,6 +1,6 @@
-import { blueGrey } from "material-ui-colors";
-import { DarkTheme as DefaultTheme } from "react-native-paper";
-import { Theme } from "react-native-paper/lib/typescript/types";
+import { blueGrey, cyan, deepOrange } from 'material-ui-colors'
+import { DarkTheme as DefaultTheme } from 'react-native-paper'
+import { Theme } from 'react-native-paper/lib/typescript/types'
 
 export const darkTheme: Theme = {
   ...DefaultTheme,
@@ -11,5 +11,14 @@ export const darkTheme: Theme = {
     ...DefaultTheme.colors,
     background: blueGrey[700],
     text: blueGrey[50],
+    primary: cyan[400],
+    accent: deepOrange[200],
+    surface: '#f00',
+    error: '#f00',
+    onSurface: '#f00',
+    disabled: '#f00',
+    placeholder: '#f00',
+    backdrop: '#f00',
+    notification: '#f00',
   },
-};
+}
